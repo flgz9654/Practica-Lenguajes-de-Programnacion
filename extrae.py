d1ec3e34c3d46326ab42a145e52189b888aee25c
Francisco Garcia <fran9415@hotmail.com>
2022-10-20 09:45:57 -0500

    Agregando comentarios explicativos

--- extrae.py
+++ extrae.py
@@ -1,9 +1,13 @@
 # Codigo para extraer un substring
 
+# Primer metodo para extraer un substring en un string definido
+
 string1 = 'Seremos los mejores en programacion'
 substring = string1[8:19]
 
 print(substring, 'en Python')
+
+# Segundo metodo para extraer solicitando una frase por pantalla
 
 frase = input('Ingresa una frase: ')
 print('Digita la posicion inicial a extraer: ')
